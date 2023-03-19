d50d31643ed021e3e66590c4fe053e81c6181e7e
Sheikh Md. Mahedi Hasan <78357062+Mahedi-Hasan10@users.noreply.github.com>
2023-03-19 22:48:45 +0600

    Add files via upload

---
+++ Week2(BFS,DFS,Recursion)/Module6(Lab)/FibonacchiUsingRecursion.cpp
@@ -0,0 +1,25 @@
+/*
+     Author : Mahedi-Hasan10
+     Date : 17-03-2023
+     Topic: Fibonacchi Using Recursion
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int fibo(int n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+
+    return fibo(n - 1) + fibo(n - 2);
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout << fibo(n) << "\n";
+
+    return 0;
+}
