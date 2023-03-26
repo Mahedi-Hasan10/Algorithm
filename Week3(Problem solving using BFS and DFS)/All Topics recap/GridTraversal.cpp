e4c81697ecf00b639ed5f3225ec4de044b5d4152
Sheikh Md. Mahedi Hasan <78357062+Mahedi-Hasan10@users.noreply.github.com>
2023-03-26 22:10:42 +0600

    Add files via upload

---
+++ Week3(Problem solving using BFS and DFS)/All Topics recap/GridTraversal.cpp
@@ -0,0 +1,132 @@
+/*
+     Author : Mahedi-Hasan10
+     Date : 26-03-2023
+     Topic: find the shortest path from the source to the destination using grid traversal
+*/
+
+/*
+5 8
+########
+#.A#...#
+#.##.#B#
+#......#
+########
+
+
+// the adjacent of the src will be --->
+(x,y) --> right = (x,y+1)
+          left = (x,y-1)
+          top = (x-1, y)
+          bottom = (X+1, y)
+
+x  y
+-  -
+0  1
+0  -1
+-1  0
+1   0
+ dx = {0,0,-1,1}
+ dy = {1,-1,0,0}
+
+
+1.is the cell within the maze?
+2. is the cell visited?
+3. is the cell forbidden?
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int n, m;
+const int N = 2000;
+int maze[N][N], visited[N][N];
+int level[N][N];
+
+int dx[] = {0, 0, -1, 1};
+int dy[] = {1, -1, 0, 0};
+
+bool is_inside(pair<int, int> coord)
+{
+    int x = coord.first;
+    int y = coord.second;
+    if (x >= 0 && x < n && y >= 0 && y < m)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool is_safe(pair<int, int> coord)
+{
+    int x = coord.first;
+    int y = coord.second;
+    if (maze[x][y] == -1)
+    {
+        return false;
+    }
+    return true;
+}
+void bfs(pair<int, int> src)
+{
+    queue<pair<int, int>> q;
+    visited[src.first][src.second] = 1;
+    level[src.first][src.second] = 0;
+    q.push(src);
+
+    while (!q.empty())
+    {
+        pair<int, int> head = q.front();
+        q.pop();
+        int x = head.first;
+        int y = head.second;
+        for (int i = 0; i < 4; i++)
+        {
+            int new_x = x + dx[i];
+            int new_y = y + dy[i];
+
+            pair<int, int> adj_node = {new_x, new_y};
+            if (is_inside(adj_node) && is_safe(adj_node) && visited[new_x][new_y] == 0)
+            {
+                visited[new_x][new_y] = 1;
+                level[new_x][new_y] = level[x][y] + 1;
+                q.push(adj_node);
+            }
+        }
+    }
+}
+int main()
+{
+    cin >> n >> m;
+    pair<int, int> src, dst;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            level[i][j] = -1;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        string input;
+        cin >> input;
+        for (int j = 0; j < m; j++)
+        {
+            if (input[j] == '#')
+                maze[i][j] = -1;
+            else if (input[j] == 'A')
+                src = {i, j};
+            else if (input[j] == 'B')
+                dst = {i, j};
+        }
+    }
+
+    bfs(src);
+
+    if (level[dst.first][dst.second] == -1)
+        cout << "NO\n";
+    else
+    {
+        cout << "YES\n";
+        cout << level[dst.first][dst.second] << "\n";
+    }
+    return 0;
+}
