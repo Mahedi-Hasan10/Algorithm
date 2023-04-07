3c169f9ed61b457171e795c593fdf346f250e0b0
Sheikh Md. Mahedi Hasan <78357062+Mahedi-Hasan10@users.noreply.github.com>
2023-04-07 19:26:56 +0600

    Add files via upload

---
+++ Week5 (Recap )/Path Printing & Dijkstra/dijkstra.cpp
@@ -0,0 +1,68 @@
+/*
+     Author : Mahedi-Hasan10
+     Date : 07-04-2023
+     Topic: Dijkstra Algorithm Simulation
+*/
+#include <bits/stdc++.h>
+using namespace std;
+const int INF = 1e9;
+const int N = 2002;
+vector<pair<int, int>> adj_list[N];
+int d[N], visited[N];
+int nodes, edges;
+
+void dijkstra(int src)
+{
+    for (int i = 1; i <= nodes; i++)
+    {
+        d[i] = INF;
+    }
+    d[src] = 0;
+
+    for (int i = 1; i <= nodes; i++)
+    {
+        int selected_node = -1;
+        for (int j = 1; j <= nodes; j++)
+        {
+            if (visited[j] == 1)
+                continue;
+            if (visited[selected_node] == -1 || d[selected_node] > d[j])
+            {
+                selected_node = j;
+            }
+        }
+        visited[selected_node] = 1;
+
+        for (auto adj_entry : adj_list[selected_node])
+        {
+            int adj_node = adj_entry.first;
+            int edge_cst = adj_entry.second;
+
+            if (d[selected_node] + edge_cst < d[adj_node])
+            {
+                d[adj_node] = d[selected_node] + edge_cst;
+            }
+        }
+    }
+}
+int main()
+{
+    cin >> nodes, edges;
+    for (int i = 0; i < edges; i++)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj_list[u].push_back({v, w});
+        adj_list[v].push_back({u, w});
+    }
+
+    int src = 1;
+    dijkstra(src);
+
+    for (int i = 1; i <= nodes; i++)
+    {
+        cout << d[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
